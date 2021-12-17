83c93bf9215c934c6b2f602d6a1e7c75fb299609
Zeronos <boss16805@gmail.com>
2021-12-17 18:06:31 +0700

    lab11_3 completed

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,30 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <cstdlib>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    ifstream dat;
+    dat.open("score.txt");
+    int n = 0;
+    string txt;
+    double sum = 0,sumdev = 0,mean,dev,datsq;
+    while(getline(dat,txt)){
+        sum += atof(txt.c_str());
+        datsq = atof(txt.c_str())*atof(txt.c_str());
+        sumdev += datsq ;
+        n++;
+    }
+    mean = sum/n;
+    dev = sqrt((sumdev/n)-(mean*mean));
+    cout << "Number of data = " << n << "\n";
+    cout << setprecision(3);
+    cout << "Mean = " << mean << "\n";
+    cout << "Standard deviation = " << dev;
+    dat.close();
+    return 0;
+}
